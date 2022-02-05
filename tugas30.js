88194bb834f8d4e385a1a625748c0c646e2f7754
rizki wahyudie git <rizkiwahyudie@gmail.com>
2022-02-05 22:11:32 +0700

    add file

---
+++ tugas30.js
@@ -0,0 +1,6 @@
+function panggilRegexp(value) {
+   var ambilData = value.match(/[es]/g)
+   console.log(ambilData)
+}
+
+panggilRegexp("Para ketua adat mendiskusikan masalah kejahatan yang terjadi di rumah adat");
